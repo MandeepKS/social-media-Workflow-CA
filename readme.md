a8b7a5d5cadb9e9ec2a3a9b91225277573816e2d
MandeepKS <129056226+MandeepKS@users.noreply.github.com>
2024-08-11 22:16:42 +0200

    Install jest and configures eslint

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Noroff Workflow CA
+
+This project is a fork of the [Noroff Social Media Client repo](https://github.com/noroffFEU/social-media-client) which adds
+
+-ESLINT
+-Jest
---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,14 @@
+//Test
+
+// describe("Login", () =>{
+
+//     // Reset mocks
+//     afterEach(() =>{
+//         jest.restAllMocks();
+//     });
+// });
+
+test('It adds 1 and 2 and gets 3 as a result', () => {
+    expect(1 + 2).toEqual(3);
+  });
+  
